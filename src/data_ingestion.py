937875c92730c5a57903437d28fddec95f49cf7c
Deepak <deepak.gecatech@gmail.com>
2026-07-14 14:41:12 +0530

    file path added for input dataset

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,11 +1,16 @@
 import pandas as pd
 from src.logger import logger
+import os
 
 def load_data():
     logger.info("Loading data by logger")
     print("Loading data...")
 
-    data = pd.read_csv("data\Sample.csv")
+    # data = pd.read_csv("data/Sample.csv")
+    
+
+    file_path = os.path.join("data", "Sample.csv")
+    data = pd.read_csv(file_path)
 
 
     return data
