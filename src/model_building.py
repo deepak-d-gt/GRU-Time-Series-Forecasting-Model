6cc88d3c9b28a2462b5d64539b5cbbb6bfc14f2b
Deepak <deepak.gecatech@gmail.com>
2026-07-14 14:50:10 +0530

    Added os.makedir in the model_building.py

--- src/model_building.py
+++ src/model_building.py
@@ -1,5 +1,23 @@
+# from sklearn.linear_model import LinearRegression
+# import joblib
+
+# def train_model(X_train, y_train):
+
+#     print("Training model...")
+
+#     model = LinearRegression()
+
+#     model.fit(X_train, y_train)
+
+#     joblib.dump(model, "models/model.pkl")
+
+#     print("Model Build Successfully!")
+
+#     return model
+
 from sklearn.linear_model import LinearRegression
 import joblib
+import os
 
 def train_model(X_train, y_train):
 
@@ -8,6 +26,8 @@ def train_model(X_train, y_train):
     model = LinearRegression()
 
     model.fit(X_train, y_train)
+
+    os.makedirs("models", exist_ok=True)
 
     joblib.dump(model, "models/model.pkl")
 
